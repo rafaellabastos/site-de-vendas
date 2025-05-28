8e6907e2928f46c295de01a174d766942fbe554b
rafaellabastos <rafa.bastos@yahoo.com.br>
2025-05-28 16:21:30 -0300

    Rodapé finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   imports: 
     [RouterOutlet, 
-    CabecalhoComponent],
+    CabecalhoComponent,
+    RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
