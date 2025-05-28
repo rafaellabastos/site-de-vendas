3cc16f6bab9a71bc8b136af328401e0c2fc9e954
rafaellabastos <rafa.bastos@yahoo.com.br>
2025-05-28 12:21:16 -0300

    Chamada, criação e estilização do cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: 
+    [RouterOutlet, 
+    CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
