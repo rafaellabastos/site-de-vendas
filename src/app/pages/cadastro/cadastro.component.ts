15f2aa9fcd637ea277a287d6c1c26e361af956a1
rafaellabastos <rafa.bastos@yahoo.com.br>
2025-05-29 16:56:19 -0300

    Tela de login e cadastro

---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+
+export class CadastroComponent {
+
+}
